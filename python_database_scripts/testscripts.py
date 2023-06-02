e21beddcb8c6dfd430b35caf72f284aaf36ea66d
yash1th25 <135289833+yash1th25@users.noreply.github.com>
2023-06-02 16:53:21 -0400

    Update testscripts.py

--- python_database_scripts/testscripts.py
+++ python_database_scripts/testscripts.py
@@ -1,13 +1,13 @@
 import csv
 import requests
 import os
-database_url = os.environ.get('DATABASE_URL', 'python_database_scripts/data.csv')
+API_url = os.environ.get('API_URL', 'python_database_scripts/data.csv')
 csvurl = os.environ.get('CSV_URL', 'http://74.64.123.44:8080/v1/users')
 
-with open(database_url) as csv_file:
+with open(csvurl) as csv_file:
     csv_reader = csv.reader(csv_file, delimiter=',')
     line_count = 0
-    url = csvurl
+    url = API_url
     for row in csv_reader:
         if line_count == 0:
             line_count += 1
